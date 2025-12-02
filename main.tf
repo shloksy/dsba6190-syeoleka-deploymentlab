d6c4a218f8744fd65b2e736741ca0645d52fecf7
shloksy <162043636+shloksy@users.noreply.github.com>
2025-12-02 18:50:33 -0500

    Refactor main.tf to update resource group name

--- main.tf
+++ main.tf
@@ -24,7 +24,8 @@ resource "random_integer" "deployment_id_suffix" {
 // Resource Group
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
+  #name     = "rg-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
+  name      = "rg-dsba6190-beta-eastus-001"
   location = var.location
 
   tags = local.tags
@@ -42,3 +43,4 @@ resource "azurerm_storage_account" "storage" {
 
   tags = local.tags
 }
+
