ad16759fc549e8e7e5a0195db4ab430477ebe10a
shloksy <162043636+shloksy@users.noreply.github.com>
2025-12-02 18:54:23 -0500

    Refactor main.tf to restore and format resource definitions

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "azurerm_resource_group" "rg" {
 // Storage Account
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "sto${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  name                     = "stodsba6190beta"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -42,5 +42,6 @@ resource "azurerm_storage_account" "storage" {
 
   tags = local.tags
 }
+
 
 
