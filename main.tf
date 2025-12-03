2fc41dbeeb9c9fabfbca796546ea78fd784cdad8
shlok-yeolekar <162043636+shloksy@users.noreply.github.com>
2025-12-03 16:00:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,12 +40,6 @@ resource "azurerm_storage_account" "storage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  network_rules {
-    default_action             = "Deny"
-    virtual_network_subnet_ids = [azurerm_subnet.sn.id]
-    bypass                     = ["AzureServices"]
-  }
-
   tags = local.tags
 }
 
