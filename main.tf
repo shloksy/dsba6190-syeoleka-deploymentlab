007bcc6cf65bcf052da69e69e7a953f0c9cef75f
shloksy <162043636+shloksy@users.noreply.github.com>
2025-12-02 20:20:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "azurerm_resource_group" "rg" {
 // Storage Account
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "stodsba6190syeolekadev${random_integer.deployment_id_suffix.result}"
+  name                     = "stodsba6190ssydev${random_integer.deployment_id_suffix.result}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -91,6 +91,7 @@ resource "azurerm_mssql_virtual_network_rule" "vn_rule" {
   server_id = azurerm_mssql_server.server.id
   subnet_id = azurerm_subnet.sn.id
 }
+
 
 
 
