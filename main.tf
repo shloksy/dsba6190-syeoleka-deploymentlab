0b983cdb07bc6bc6be7beffbf688e59bd7883baf
shloksy <162043636+shloksy@users.noreply.github.com>
2025-12-02 20:06:09 -0500

    Add Azure SQL Server and Database resources

--- main.tf
+++ main.tf
@@ -43,5 +43,47 @@ resource "azurerm_storage_account" "storage" {
   tags = local.tags
 }
 
+resource "azurerm_mssql_server" "server" {
+  name                         = "sql-dsba6190-syeoleka-dev-${random_integer.deployment_id_suffix.result}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  # administrator_login          = "4dm1n157r470r"
+  # administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_mssql_database" "db" {
+  name         = "db-dsba6190-syeoleka-dev-${random_integer.deployment_id_suffix.result}"
+  server_id    = azurerm_mssql_server.server.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "S0"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-dsba6190-syeoleka-dev-${random_integer.deployment_id_suffix.result}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "sn" {
+  name                 = "subnet-dsba6190-syeoleka-dev-${random_integer.deployment_id_suffix.result}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/16"]
+  service_endpoints    = ["Microsoft.Sql"]
+}
+
+resource "azurerm_mssql_virtual_network_rule" "vn_rule" {
+  name      = "vnet-rule-dsba6190-syeoleka-dev-${random_integer.deployment_id_suffix.result}"
+  server_id = azurerm_mssql_server.server.id
+  subnet_id = azurerm_subnet.sn.id
+}
 
 
