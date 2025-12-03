20a067988f5637ca93264fb8f3561fcbf6f4a827
shloksy <162043636+shloksy@users.noreply.github.com>
2025-12-03 14:21:58 -0500

    Fix formatting and update SQL server SKU to Basic

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "azurerm_mssql_database" "db" {
   collation    = "SQL_Latin1_General_CP1_CI_AS"
   license_type = "LicenseIncluded"
   max_size_gb  = 2
-  sku_name     = "S0"
+  sku_name     = "Basic"
 
   tags = local.tags
 
@@ -91,6 +91,7 @@ resource "azurerm_mssql_virtual_network_rule" "vn_rule" {
   server_id = azurerm_mssql_server.server.id
   subnet_id = azurerm_subnet.sn.id
 }
+
 
 
 
