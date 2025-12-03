b408a4add2931dda3a57ff4d3222c62b832b3d32
shloksy <162043636+shloksy@users.noreply.github.com>
2025-12-02 20:17:45 -0500

    Refactor main.tf for resource naming and tags

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "random_integer" "deployment_id_suffix" {
 // Resource Group
 
 resource "azurerm_resource_group" "rg" {
-  name      = "rg-dsba6190-beta-eastus-001"
+  name      = "rg-dsba6190-syeoleka-eastus-dev-${random_integer.deployment_id_suffix.result}"
   location = var.location
 
   tags = local.tags
@@ -34,7 +34,7 @@ resource "azurerm_resource_group" "rg" {
 // Storage Account
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "stodsba6190beta"
+  name                     = "stodsba6190syeolekadev${random_integer.deployment_id_suffix.result}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -50,6 +50,8 @@ resource "azurerm_mssql_server" "server" {
   version                      = "12.0"
   administrator_login          = "4dm1n157r470r"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+
+  tags = local.tags
 }
 
 resource "azurerm_mssql_database" "db" {
@@ -59,6 +61,8 @@ resource "azurerm_mssql_database" "db" {
   license_type = "LicenseIncluded"
   max_size_gb  = 2
   sku_name     = "S0"
+
+  tags = local.tags
 
   lifecycle {
     prevent_destroy = true
@@ -70,6 +74,8 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  
+  tags = local.tags
 }
 
 resource "azurerm_subnet" "sn" {
@@ -78,13 +84,18 @@ resource "azurerm_subnet" "sn" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.0.0/16"]
   service_endpoints    = ["Microsoft.Sql"]
+  
+tags = local.tags
 }
 
 resource "azurerm_mssql_virtual_network_rule" "vn_rule" {
   name      = "vnet-rule-dsba6190-syeoleka-dev-${random_integer.deployment_id_suffix.result}"
   server_id = azurerm_mssql_server.server.id
   subnet_id = azurerm_subnet.sn.id
+
+  tags = local.tags
 }
+
 
 
 
